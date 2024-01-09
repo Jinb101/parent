8cea07bcd44b617d83db1f1fec41a499b03f14f2
Jinb <1312112285@qq.com>
2024-01-09 17:02:24 +0800

    flx:text_pre_wrap

--- src/views/index/notice/detail.vue
+++ src/views/index/notice/detail.vue
@@ -12,31 +12,33 @@
         {{ appTimeout(detail.time) }}
       </span>
     </div>
-    <div class="content app_frame_content" v-html="detail.content"></div>
+    <div class="content app_frame_content"
+         style="white-space: pre-wrap"
+         v-html="detail.content"></div>
     <div class="images flex fw">
-      <div
-        class="ava"
-        v-for="(i, j) in detail.imgs"
-        :key="j + '-i'"
-        @click="appShowImage(detail.imgs, j, 'img')"
-      >
+      <div class="ava"
+           v-for="(i, j) in detail.imgs"
+           :key="j + '-i'"
+           @click="appShowImage(detail.imgs, j, 'img')">
         <van-image :src="i.img"></van-image>
       </div>
     </div>
-    <van-button
-      v-if="detail.is_sign > 0 && !detail.sign"
-      size="small"
-      type="primary"
-      @click="write = true"
-      >签名</van-button
-    >
+    <van-button v-if="detail.is_sign > 0 && !detail.sign"
+                size="small"
+                type="primary"
+                @click="write = true">签名</van-button>
     <div v-if="detail.sign">
       <h5>我的签名</h5>
-      <van-image :src="detail.sign" class="sign"></van-image>
+      <van-image :src="detail.sign"
+                 class="sign"></van-image>
     </div>
     <transition name="van-slide-left">
-      <div class="app_show_fixed top" style="z-index: 580" v-if="write">
-        <v-tool-title text="签名" back @on-back="write = false"></v-tool-title>
+      <div class="app_show_fixed top"
+           style="z-index: 580"
+           v-if="write">
+        <v-tool-title text="签名"
+                      back
+                      @on-back="write = false"></v-tool-title>
         <div class="views">
           <v-h @on-save="onSave"></v-h>
         </div>
@@ -88,29 +90,36 @@ export default {
 .index_notice_detail {
   overflow-y: scroll;
   padding: 8px;
+
   .sign {
     height: 200px;
   }
+
   .title {
     font-weight: 700;
     padding: 10px 0;
     line-height: 1.2;
     margin-bottom: 2px;
   }
+
   .tool {
     font-size: 14px;
     color: #999;
     margin-bottom: 5px;
+
     .van-icon {
       vertical-align: middle;
     }
-    span + span {
+
+    span+span {
       margin-left: 20px;
     }
   }
+
   .content {
     padding: 8px 0;
   }
+
   .images {
     .ava {
       width: 100px;
@@ -120,5 +129,4 @@ export default {
       overflow: hidden;
     }
   }
-}
-</style>
+}</style>
